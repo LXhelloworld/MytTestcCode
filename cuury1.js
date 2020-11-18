4c417b2e7def7702803183791218dad43f47c2ff
LXhelloworld <lixiao4193@163.com>
2020-11-18 20:03:51 +0800

    柯里化

---
+++ cuury1.js
@@ -0,0 +1,7 @@
+const _ = require('lodash');
+
+const array = ['jack1','jack2','jack3','jack4','jack5','jack6'];
+
+console.log(_.first(array))
+console.log(_.toUpper(array))
+console.log(_.reverse(array))
