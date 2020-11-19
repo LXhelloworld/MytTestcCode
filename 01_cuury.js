f45bafd66134e99c9a47ec452dd1bd65fbfe9217
LXhelloworld <lixiao4193@163.com>
2020-11-19 11:17:42 +0800

    函数柯里化

--- 01_cuury.js
+++ 01_cuury.js
@@ -9,5 +9,15 @@ const haveSpace = match(/\s+/g);
 
 const haveNumber = match(/\d+/g);
 
-console.log(haveSpace('hello world'))
-console.log(haveNumber('hello world'))
+//console.log(haveSpace('hello world'))
+// console.log(haveNumber('hello world'))
+
+const filter = _.curry(function(func, arr){
+    return arr.filter(func)
+})
+
+const array = ['jack 1','jack 2','jack3','jack4','jack 5','jack6'];
+
+const findSpace = filter(haveSpace)
+
+console.log(findSpace(array))
--- cuury1.js
+++ cuury1.js
@@ -15,8 +15,19 @@ function getSum(a, b, c){
 
 //console.log(getSum(4,2,4));
 
-const curried = _.curry(getSum);
+const curried = curry(getSum);
 
 console.log(curried(1,2,3))
-console.log(curried(1)(2)(5))
+console.log(curried(1,2)(5))
 
+function curry(func){
+    return function curriedFn(...args){
+        //判断实参和形参的个数
+        if(args.length <func.length){
+            return function (){
+                return curriedFn(...args.concat(Array.from(arguments)))
+            }
+        }
+        return func(...args)
+    }
+}
